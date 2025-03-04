33dac7b739f97552e4bd8d8b074ccb75967706a7
mydkong <contact.donkeykong@protonmail.com>
2025-03-04 18:11:59 +0100

    Update script.js

--- UJ/byUJ/pendu/script.js
+++ UJ/byUJ/pendu/script.js
@@ -1,5 +1,5 @@
 // @bugfix arrêt de la boucle infinie while //
-var motsFetches, words, mots;
+var motsFetches, words=[], mots;
 async function getFrenchWords() {
   try {
     const response = await fetch("https://trouve-mot.fr/api/random/3574");
