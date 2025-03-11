0945bb4b3281b3ffc784573a805eff20253c5ffa
mydkong <contact.donkeykong@protonmail.com>
2025-03-11 20:06:28 +0100

    Update main.js

--- UJ+/make-profile/main.js
+++ UJ+/make-profile/main.js
@@ -10,13 +10,24 @@ const nextstep=(st)=>{
       <button class="next-step">Continuer</button>`;
     for(var i=0;i<6;i++){
       event.select(".img-pp").innerHTML+='<button class="button-icon'+i+'><img class="icon-pp icon'+i+'" src="img/icon'+i+'.png"></button>';
+      event.select(".button-icon"+i).addEventListener("click", ()=>{
+        iconface=String(i);
+      });
     }
   }else if(st===2){
-    event.select("")//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////ICI
     event.select(".root").innerHTML=`<h1>Étape 2 : Votre nom</h1>
     <p class="subtitle">Entrez le nom par lequel vous voulez que l'on vous appelle</p>
     <input type="text" class="input-name">
     <button class="next-step">Continuer</button>`;
+  }else if(st===3){
+    name=event.select(".input-name").value;
+    const dataLS=new Object();
+    dataLS[sessionStorage.getItem("IDUSER")]=new Object();
+    dataLS[sessionStorage.getItem("IDUSER")].iconface=iconface;
+    dataLS[sessionStorage.getItem("IDUSER")].name=name;
+    localStorage.setItem("UJ+data", data);
+    event.select(".root").innerHTML=`<h1>Tout est prêt !</h1>
+    <button class="link-uj+">Aller sur UJ+</button>`;
   }
 }
 
