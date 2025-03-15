20d3a353b8463322486b1ff0562e202ff2e4a376
mydkong <contact.donkeykong@protonmail.com>
2025-03-15 16:41:40 +0100

    Update main.js

--- UJ+/main.js
+++ UJ+/main.js
@@ -6,26 +6,26 @@ event.select=(str)=>{
 if(sessionStorage.getItem("IDUSER")===null){
   window.location.href="login/";
 }else{
-  document.querySelector(".root").innerHTML=`<ul class="header-options">
+  document.querySelector(".root").innerHTML=`<ol class="header-options">
   <li class="img-in-header">
     <img src="/home/paramDkong/images/petitesIcones/UJplus.png">
   </li>
   <li class="deconnexion-button">
     Déconnexion
-    <ul class="deconnexion-options-menu" hidden>
+    <ol class="deconnexion-options-menu" hidden>
       <li class="partial-deconnexion">Se déconnecter</li>
       <li class="complete-deconnexion">Se déconnecter sans rien laisser sur le navigateur</li>
-    </ul>
+    </ol>
   </li>
-  </ul>`;
-  event.select("deconnexion-options").addEventListener("click", ()=>{
-    event.select("deconnexion-options-menu").hidden=!event.select("deconnexion-options-menu").hidden;
+  </ol>`;
+  document.querySelector(".deconnexion-button").addEventListener("click", ()=>{
+    document.querySelector(".deconnexion-options-menu").hidden=!document.querySelector(".deconnexion-options-menu").hidden;
   });
-  event.select("partial-deconnexion").addEventListener("click", ()=>{
+  document.querySelector(".partial-deconnexion").addEventListener("click", ()=>{
     sessionStorage.removeItem("IDUSER");
     window.location.reload();
   });
-  event.select("complete-deconnexion").addEventListener("click", ()=>{
+  document.querySelector(".complete-deconnexion").addEventListener("click", ()=>{
     sessionStorage.removeItem("IDUSER");
     localStorage.removeItem("UJ+data");
     window.location.reload();
