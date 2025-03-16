2c09d902367ce8921a75f69539902ef826a59d9a
mydkong <contact.donkeykong@protonmail.com>
2025-03-16 18:20:12 +0100

    Update main.js

--- UJ+/main.js
+++ UJ+/main.js
@@ -17,7 +17,22 @@ if(sessionStorage.getItem("IDUSER")===null){
       <li class="complete-deconnexion">Se déconnecter sans rien laisser sur le navigateur</li>
     </ol>
   </li>
-  </ol>`;
+  </ol>
+  <h1>Geometry Dash</h1>
+  <div class="flexbox">
+    <a href="choose-level-of-gd">
+      <img src="gd-normal.png"><br>
+      Geometry Dash
+    </a>
+    <a href="gd?p=216343253&p=t>
+      <img src="gd-subzero.png"><br>
+      Geometry Dash Subzero
+    </a>
+    <a href="gd?p=143663274&p=t>
+      <img src="gd-meltdown.png"><br>
+      Geometry Dash Meltdown
+    </a>
+  </div>`;
   document.querySelector(".deconnexion-button").addEventListener("click", ()=>{
     document.querySelector(".deconnexion-options-menu").hidden=!document.querySelector(".deconnexion-options-menu").hidden;
   });
