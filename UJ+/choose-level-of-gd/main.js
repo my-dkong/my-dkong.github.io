4e3823cc3d802790982d10d2df6f160b5c1ceea0
mydkong <contact.donkeykong@protonmail.com>
2025-03-16 18:47:43 +0100

    Create comment

---
+++ UJ+/choose-level-of-gd/main.js
@@ -0,0 +1,6 @@
+var event=new Object();
+event.select=(str)=>{
+  return document.querySelector(str);
+}
+
+/* @note vérifier si l'utilisateur est bien connecté et si oui afficher les différents niveaux */
