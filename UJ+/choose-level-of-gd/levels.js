8a924508f8e0be03a3011030c6a860c0a023d7e6
mydkong <contact.donkeykong@protonmail.com>
2025-03-17 18:42:59 +0100

    Create levels.js

---
+++ UJ+/choose-level-of-gd/levels.js
@@ -0,0 +1,40 @@
+/* @note insert levels constant for main.js */
+
+const levels={
+	"content": {
+	  "name": "Geometry Dash",
+		"levels": [
+			{
+				"id": 105500895,
+				"levels": [1, 2, 3],
+				"author": "griffpatch"
+			},
+			{
+				"id": 118638917,
+				"levels": [4, 5, 6],
+				"author": "hoppingicon"
+			},
+			{
+				"id": 120721851,
+				"levels": [7, 8],
+				"author": "hoppingicon"
+			},
+			{
+				"id": 140185501,
+				"levels": [9, 10, 11, 12, 13],
+				"author": "CrystalKeeper7"
+			},
+			{
+				"id": 194164289,
+				"levels": [14, 15],
+				"author": "griffpatch"
+			},
+			{
+				"id": 194895616,
+				"levels": [16, 17, 18, 19, 20, 21],
+				"author": "chikomastr"
+			}
+		]
+	},
+	"CreatorOfGroup": "hoppingicon"
+}
