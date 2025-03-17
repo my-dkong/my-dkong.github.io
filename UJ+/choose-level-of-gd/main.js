11759ad204435f79dbadf73154baaf3b9f2654df
mydkong <contact.donkeykong@protonmail.com>
2025-03-17 18:31:33 +0100

    Update main.js

--- UJ+/choose-level-of-gd/main.js
+++ UJ+/choose-level-of-gd/main.js
@@ -4,3 +4,8 @@ event.select=(str)=>{
 }
 
 /* @note vérifier si l'utilisateur est bien connecté et si oui afficher les différents niveaux */
+if(sessionStorage.getItem("IDUSER")!==null){
+  event.select(".root").innerHTML=`<h1>Choisissez votre niveau Geometry Dash</h1>
+  <div class="levels"></div>`;
+  for(/// faire for() avec le document inséré dans index.html ///
+}
