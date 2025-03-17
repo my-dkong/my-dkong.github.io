09e86915a03e2b1761a389db86d4b609685a134b
mydkong <contact.donkeykong@protonmail.com>
2025-03-17 19:01:42 +0100

    Update main.js

--- UJ+/choose-level-of-gd/main.js
+++ UJ+/choose-level-of-gd/main.js
@@ -7,5 +7,10 @@ event.select=(str)=>{
 if(sessionStorage.getItem("IDUSER")!==null){
   event.select(".root").innerHTML=`<h1>Choisissez votre niveau Geometry Dash</h1>
   <div class="levels"></div>`;
-  for(/// faire for() avec le document inséré dans index.html ///
+  for(var i=0;i<levels.content.levels.length;i++){
+    event.select(".levels").innerHTML+='<div class="gd-box-level"><img class="gd-img'+i+'" src="img-gd/icon'+i+'.png" alt="Icône Geometry Dash n°'+(i+1)+'"></div>';
+    event.select(".gd-img"+i).addEventListener("click", ()=>{
+      window.location.href="/UJ+/gd?l="+(i+1)+"/";
+    });
+  }
 }
