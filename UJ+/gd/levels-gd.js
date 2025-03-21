58d41daa9e662cf536bc6080edc2c53ad7c6dbce
mydkong <contact.donkeykong@protonmail.com>
2025-03-21 18:43:12 +0100

    Create levels-gd.js

---
+++ UJ+/gd/levels-gd.js
@@ -0,0 +1,60 @@
+const levels={
+	"content": [
+		{
+			"name": "Geometry Dash",
+			"levels": [
+				{
+					"id": 105500895,
+					"levels": [1, 2, 3],
+					"author": "griffpatch"
+				},
+				{
+					"id": 118638917,
+					"levels": [4, 5, 6],
+					"author": "hoppingicon"
+				},
+				{
+					"id": 120721851,
+					"levels": [7, 8],
+					"author": "hoppingicon"
+				},
+				{
+					"id": 140185501,
+					"levels": [9, 10, 11, 12, 13],
+					"author": "CrystalKeeper7"
+				},
+				{
+					"id": 194164289,
+					"levels": [14, 15],
+					"author": "CrystalKeeper7"
+				},
+				{
+					"id": 194895616,
+					"levels": [16, 17, 18, 19, 20, 21],
+					"author": "chikomastr"
+				}
+			]
+		},
+		{
+			"name": "Geometry Dash Subzero",
+			"levels": [
+				{
+					"id": 216343253,
+					"levels": [1, 2, 3],
+					"author": "CrystalKeeper7"
+				}
+			]
+		},
+		{
+			"name": "Geometry Dash Meltdown",
+			"levels": [
+				{
+					"id": 143663274,
+					"levels": [1, 2, 3],
+					"author": "CrystalKeeper7"
+				}
+			]
+		}
+	],
+	"CreatorOfGroup": "hoppingicon"
+}
