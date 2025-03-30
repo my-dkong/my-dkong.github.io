3dec6c9710d734c87f98696c4719d2435715ac25
mydkong <contact.donkeykong@protonmail.com>
2025-03-30 12:38:33 +0200

    Update main.js

--- UJ+/main.js
+++ UJ+/main.js
@@ -74,7 +74,7 @@ if(sessionStorage.getItem("IDUSER")===null){
       body: JSON.stringify(message)
     })
     .then(response => alert("Message envoyé !"))
-    .catch(error => alert("Erreur :", error));
+    .catch(error => alert("Erreur :"+error));
   });
 	document.querySelector(".show-hide-return").addEventListener("click", ()=>{
 		document.querySelector(".box-return").hidden=!document.querySelector(".box-return").hidden; // @note fait basculer la visibilité du champ de saisie //
